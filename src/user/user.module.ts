79fbb3edcccd1ef7d9b01f0167873be416bccd49
vbelchior <victor@thau.tech>
2021-12-16 19:04:48 -0300

    Create User Service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
+import { UserService } from './user.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), TypeOrmModule.forFeature([UserEntity])],
   controllers: [],
-  providers: [],
+  providers: [UserService],
   exports: [],
 })
 export class UserModule {}
