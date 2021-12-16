f55c31c75a282e21d848bfa9f332726d2b2dfaaf
vbelchior <victor@thau.tech>
2021-12-16 19:21:00 -0300

    Insert Controler on Module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './user.controller';
 import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), TypeOrmModule.forFeature([UserEntity])],
-  controllers: [],
+  controllers: [UserController],
   providers: [UserService],
   exports: [],
 })
